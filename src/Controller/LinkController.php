6b58889f52709cc64b085010dadacf09b88ce606
isaias <isaias-araujo1@hotmail.com>
2023-02-01 14:47:56 -0300

    dpl: add controller and entity

---
+++ src/Controller/LinkController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Link;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Mapping\Entity;
+
+class LinkController extends AbstractController
+{
+    #[Route('/', name: 'appInsert')]
+    public function inserLink(): Response
+    {
+        $name = "oláMundo";
+        return $this->render('insert.html.twig',compact('name'));
+    }
+    ## Create
+
+    #[Route('/create', name: 'appCreate', methods: ['POST'])]
+    public function create(EntityManagerInterface $em, Request $request): Response
+    {
+        $data = $request->request->all();
+
+        $link = $em->getRepository(Link::class)->find(4);
+        // $link = new Link;
+        $link->setUrl($data['url']);
+
+        $em->persist($link);
+        $em->flush();
+
+        return $this->redirectToRoute('appInsert');
+        // return $this->render('');
+    }
+
+    #[Route('/redirecionamento', name: 'appRedirecionamento')]
+    public function redir(EntityManagerInterface $em): Response
+    {
+        $link = $em->getRepository(Link::class)->find(4);
+
+        $url = $link->getUrl();
+
+        dump($url);
+        
+        return $this->render('redirect.html.twig', compact('url'));
+        
+    }
+}
